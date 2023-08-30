d4df36c16f0ebe4491fcb3fab9d0589148d66f95
szalariih <137455797+szalariih@users.noreply.github.com>
2023-08-29 21:25:39 -0300

    Add files via upload

---
+++ OlaMundo/Controllers/OlaMundoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OlaMundo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OlaMundoController : ControllerBase
+    {
+        
+        public OlaMundoController(ILogger<OlaMundoController> logger)
+        {
+           
+        }
+
+        [HttpGet]
+        public OlaMundo ObterMensagem()
+        {
+            var retorno = new OlaMundo();
+            retorno.Mensagem = "Olá mundo - Essa é minha primeira Web API";
+            retorn retorno;
+        }
+    }
+}
---
+++ OlaMundo/WeatherForecast.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace OlaMundo
+{
+    public class OlaMundo
+    {
+       public string Mensagem { get; set; } 
+    }
+}
